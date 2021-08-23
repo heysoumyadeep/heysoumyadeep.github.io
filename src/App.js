50261adadb6372e138d538414f1e1b47ac3884ce
Soumyadeep Pradhan <soumyadeeppradhan@gmail.com>
2021-08-23 21:11:04 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import './components/dev.js';
+function App() {
+  return (
+
+    <div className="temp">
+      <h1>Website is currently in development and will be live soon. </h1>
+      <p>Thank You</p>
+    </div>
+
+    // <section className="coming-soon">
+    //   <div>
+    //     <h2>Website will be live in</h2>
+    //     <div className="countdown">
+    //       <div className="container-day">
+    //         <h3 className="day">Time</h3>
+    //         <h3>Days</h3>
+    //       </div>
+    //       <div className="container-hour">
+    //         <h3 className="hour">Time</h3>
+    //         <h3>Hours</h3>
+    //       </div>
+    //       <div className="container-minute">
+    //         <h3 className="minute">Time</h3>
+    //         <h3>Minutes</h3>
+    //       </div>
+    //       <div className="container-second">
+    //         <h3 className="second">Time</h3>
+    //         <h3>Seconds</h3>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
+  );
+}
+
+export default App;
